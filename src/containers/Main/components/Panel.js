829d69df976c96b47e9b4503c890ded0dcc58779
DuMuniz <edu.smuniz@hotmail.com>
2020-08-23 22:49:11 -0300

    Alterado link de compartilhamento

--- src/containers/Main/components/Panel.js
+++ src/containers/Main/components/Panel.js
@@ -28,7 +28,7 @@ function Panel( { updateAt, onChange, data, country, getCoviddata }){
         navigator.share({
             title: `Dados do Covid-19- ${country}`,
             text: textCovid19,
-            url: 'https://covid19pwa.netlify.app'
+            url: 'https://covid19-pwa.vercel.app'
         })
     }
 
